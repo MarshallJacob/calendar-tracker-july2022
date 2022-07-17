b3013d0fce1f046cf534b38e157c8136b207b83f
Mjacke <marshall.jacob16@icloud.com>
2022-07-17 15:04:07 -0600

    Java script additions. Pulling elements from HTML. Creating loops and functions help sort that data.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
+// Pull elements from HTML
+const formControlEl = document.querySelector(".form-control").innerHTML;
+const btnEl = document.querySelectorAll("button");
+const scheduleTimeEl = document.querySelector(".scheduleTime").innerHTML;
 
-var now = moment()
+// Shows the current day
+var now = moment();
 $("#currentDay").text(now.format("dddd, MMMM Do"));
+
+console.log(moment().format("hh A"));
+console.log(scheduleTimeEl);
+function timeColorCoder () {
+    for(var i = 0; i < scheduleTimeEl.length; i++)
+    if (scheduleTimeEl = moment ().format("hh A")) {
+        console.log("current time!")
+    } else if (scheduleTimeEl < moment ().format("hh A")) {
+        console.log ("Past!")
+    } else (scheduleTimeEl > moment ().format("hh A")); {
+        console.log("Future!")
+    }return
+}
+
+for (i of btnEl) {
+    i.addEventListener("click", saveData)
+};
+
+function saveData () {
+    // write the code that will save input data from formControlEl
+    console.log("This button works!")
+};
