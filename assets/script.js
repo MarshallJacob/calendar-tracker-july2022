debb481e9a0231b71c0facb7a7524ae433bf8920
Mjacke <marshall.jacob16@icloud.com>
2022-07-20 09:11:07 -0600

    functioning save buttons tied to the correct time block.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,26 @@
 // Pull elements from HTML
-const planInput = document.querySelector(".planInput");
-const btnEl = document.querySelectorAll("button");
+// const planInput = document.querySelector(".planInput");
+// const btnEl = document.querySelectorAll("button");
 const scheduleTimeEl = document.querySelector(".scheduleTime").innerHTML;
+const nineEl = document.getElementById("9")
+const tenEl = document.getElementById("10")
+const elevenEl = document.getElementById("11")
+const twelveEl = document.getElementById("12")
+const oneEl = document.getElementById("1")
+const twoEl = document.getElementById("2")
+const threeEl = document.getElementById("3")
+const fourEl = document.getElementById("4")
+const fiveEl = document.getElementById("5")
+const btn9El = document.getElementById("btn9")
+const btn10El = document.getElementById("btn10")
+const btn11El = document.getElementById("btn11")
+const btn12El = document.getElementById("btn12")
+const btn1El = document.getElementById("btn1")
+const btn2El = document.getElementById("btn2")
+const btn3El = document.getElementById("btn3")
+const btn4El = document.getElementById("btn4")
+const btn5El = document.getElementById("btn5")
+
 
 // Shows the current day
 var now = moment();
@@ -20,14 +39,28 @@ function timeColorCoder () {
     }return
 }
 
+// event listeners for each button
+btn9El.addEventListener("click", saveData(nineEl.id))
+btn10El.addEventListener("click", saveData(tenEl.id))
+btn11El.addEventListener("click", saveData(elevenEl.id))
+btn12El.addEventListener("click", saveData(twelveEl.id))
+btn1El.addEventListener("click", saveData(oneEl.id))
+btn2El.addEventListener("click", saveData(twoEl.id))
+btn3El.addEventListener("click", saveData(threeEl.id))
+btn4El.addEventListener("click", saveData(fourEl.id))
+btn5El.addEventListener("click", saveData(fiveEl.id))
 
-for (i of btnEl) {
-    i.addEventListener("click", saveData)
-};
+// for (i of btnEl) {
+//     i.addEventListener("click", saveData)
+// };
 
-function saveData () {
+
+function saveData (timeId) {
     // write the code that will save input data from formControlEl
     
-    console.log(planInput)
-    console.log("This button works!")
+    // console.log(timeId)
+    // console.log("This button works!")
+    return (e) => {
+        console.log(timeId)
+      }
 };
