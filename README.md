19ad8eb6618493bbba81cb1fbc6dbfac29067eaf
Mjacke <marshall.jacob16@icloud.com>
2022-07-19 20:53:53 -0600

    READ ME file filled out and updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # calendar-tracker-july2022
+
+## Welcome to my calendar tracker
+
+### I have the seperate hours during a standard workday and the ability to enter in what your plans are during that hour.
+
+### Here is a screenshot of the website. 
+
+![Picture of the calender website](assets/Screenshot.png "Top of page")
+
+### 
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Pull elements from HTML
-const formControlEl = document.querySelector(".form-control").innerHTML;
+const planInput = document.querySelector(".planInput");
 const btnEl = document.querySelectorAll("button");
 const scheduleTimeEl = document.querySelector(".scheduleTime").innerHTML;
 
@@ -11,14 +11,15 @@ console.log(moment().format("hh A"));
 console.log(scheduleTimeEl);
 function timeColorCoder () {
     for(var i = 0; i < scheduleTimeEl.length; i++)
-    if (scheduleTimeEl = moment ().format("hh A")) {
+    if (scheduleTimeEl.value = moment ().format("hh A")) {
         console.log("current time!")
-    } else if (scheduleTimeEl < moment ().format("hh A")) {
+    } else if (scheduleTimeEl.value < moment ().format("hh A")) {
         console.log ("Past!")
     } else (scheduleTimeEl > moment ().format("hh A")); {
         console.log("Future!")
     }return
 }
+
 
 for (i of btnEl) {
     i.addEventListener("click", saveData)
@@ -26,5 +27,7 @@ for (i of btnEl) {
 
 function saveData () {
     // write the code that will save input data from formControlEl
+    
+    console.log(planInput)
     console.log("This button works!")
 };
